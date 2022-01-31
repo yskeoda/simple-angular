cd4e1cf2665befe5759e9856b9bf87ba65c31457
yskeoda <yskeoda@gmail.com>
2022-01-31 12:51:17 -0500

    init detail component

--- simple-app/src/app/app.module.ts
+++ simple-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailComponent } from './feature/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
