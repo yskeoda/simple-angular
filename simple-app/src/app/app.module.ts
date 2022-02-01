f1994e59c8abb20ef61a9c35436b00e82de3979f
yskeoda <yskeoda@gmail.com>
2022-01-31 22:34:48 -0500

    init summary component

--- simple-app/src/app/app.module.ts
+++ simple-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SummaryComponent } from './summary/summary.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryComponent
   ],
   imports: [
     BrowserModule,
